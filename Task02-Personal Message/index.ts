e0132ead36bd54bbe20857a2c3b90182a82555fe
Marjan-ahmed <marjan.ahmed08@yahoo.com>
2024-05-19 11:46:45 +0500

    first commit

---
+++ Task02-Personal Message/index.ts
@@ -0,0 +1,4 @@
+// QUESTION #1:
+
+let person : string = "Marjan Ahmed"; //store the value (name)
+console.log(`Hello ${person}, would you like to learn some TypeScript today?”`) //print it by y template litteral
