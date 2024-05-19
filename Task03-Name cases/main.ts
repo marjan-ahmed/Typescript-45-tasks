78bdbbf80e28efa5a1e8cfbc40182ed9ca06a951
Marjan-ahmed <marjan.ahmed08@yahoo.com>
2024-05-19 12:11:10 +0500

    first commit

---
+++ Task03-Name cases/main.ts
@@ -0,0 +1,8 @@
+// QUESTION #3:
+
+let person = "marjan";
+
+console.log(person.toUpperCase()); //MARJAN
+console.log(person.toLowerCase()); //marjan
+const titleCase = person[0].toUpperCase() + 'arjan'; 
+console.log(titleCase); //Marjan
---
+++ Task05-Famous qoute 2/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let famous_person = "Elon Musk";
+let message = "Take Risk Now. Do something bold. You won't regret it.";
+console.log(`'${famous_person} once said, "${message}"'`);
