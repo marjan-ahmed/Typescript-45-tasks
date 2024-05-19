79153caafafa783facafa3e6a11df7f2026bf901
Marjan-ahmed <marjan.ahmed08@yahoo.com>
2024-05-19 22:46:38 +0500

    first commit

---
+++ Task29-Favourite fruits/app.ts
@@ -0,0 +1,18 @@
+let favouriteFruits: string[] = ['apple', 'mango', 'orange', 'watermelon'];
+
+if(favouriteFruits.includes("apple")) {
+    console.log("I think you like apple.")
+}
+if(favouriteFruits.includes("orange")) {
+    console.log("I think you like orange.")
+}
+if(favouriteFruits.includes("mango")) {
+    console.log("I think you like mango.")
+}
+if(favouriteFruits.includes("watermelon")) {
+    console.log("I think you like watermelon.")
+}
+
+else{
+    console.log("I don't think you like any of these fruits.")
+}
