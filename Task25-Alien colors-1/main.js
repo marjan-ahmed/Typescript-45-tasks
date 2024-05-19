0a5617c2a9335518543335b235e5733e45281df8
Marjan-ahmed <marjan.ahmed08@yahoo.com>
2024-05-19 22:01:50 +0500

    first commit

---
+++ Task25-Alien colors-1/main.js
@@ -0,0 +1,10 @@
+"use strict";
+let alien_color = "green";
+if (alien_color === "green") {
+    console.log("You got 5 stars!!!");
+}
+alien_color = "red";
+if (alien_color == "red") {
+    // No output because the condition is false
+    console.log("You got 0 points");
+}
---
+++ Task26-Alien colors-2/main.js
@@ -0,0 +1,11 @@
+"use strict";
+let alien_color = "green";
+//Output Occur
+if (alien_color == "green") {
+    console.log("You just earned 5 points!");
+}
+// No Output 
+alien_color = "red";
+if (alien_color == "green") {
+    // No output because the condition is false
+}
