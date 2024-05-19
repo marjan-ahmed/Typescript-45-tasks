53b838974792ea0b845893cde0cf248a3508fe44
Marjan-ahmed <marjan.ahmed08@yahoo.com>
2024-05-19 16:38:30 +0500

    first commit

--- task18-Dinner guest/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-let guests = ["Yahya", "Haris", "Zubaida", "fatimah", "Shahid"];
-//guests.length = 5
-console.log(`I would like to invite ${guests.length} friends in my dinner`);
--- task18-Dinner guest/index.ts
+++
@@ -1,4 +0,0 @@
-let guests: string[] = ["Yahya", "Haris", "Zubaida", "fatimah", "Shahid"]
-
-//guests.length = 5
-console.log(`I would like to invite ${guests.length} friends in my dinner`)
